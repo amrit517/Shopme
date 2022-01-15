8e6b94448d2aea4143cff6dacab95ef9c0fb00a8
amrit517 <amritsingh5750@gmail.com>
2022-01-15 13:17:47 +0530

    user logout

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -44,7 +44,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests().anyRequest().authenticated()
-			.and().formLogin().loginPage("/login").usernameParameter("email").permitAll();
+			.and().formLogin().loginPage("/login").usernameParameter("email").permitAll().
+			and().logout().permitAll();
 	}
 
 	@Override
